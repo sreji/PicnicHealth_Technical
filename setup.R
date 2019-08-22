632ed076d5317fd52e16593fd3c233c509d8f0cf
sreji <sreji@stanford.edu>
2019-08-21 20:47:22 -0400

    Halfway done

--- setup.R
+++ setup.R
@@ -1,15 +1,21 @@
 library(dplyr)
+library(ggplot2)
+library(kableExtra)
+library(tidyr)
 
 ## Read in the weather data and fix the data time formatting
 ## Split the hour/min/sec for further analysis
+## Filtering the dates that are only in both weather and bike data
 weather_data = read.csv("/Users/sreji/Documents/PicnicHealth_Technical/data/sf_weather.csv") %>%
   mutate(ymd = as.Date(DATE)) %>%
+  filter(ymd >= '2017-06-28' & ymd <= '2018-05-05') %>% 
   mutate(hour = unclass(as.POSIXlt(DATE)$hour), min = unclass(as.POSIXlt(DATE)$min),sec = unclass(as.POSIXlt(DATE)$sec))
 
 ## Function to split the date time for the bike data
 split_date = function(file) {
   file %>%
     mutate(start_ymd = as.Date(start_time),end_ymd = as.Date(end_time)) %>%
+    filter(start_ymd >= '2017-06-28' & end_ymd <= '2018-05-05') %>% 
     mutate(start_hour = unclass(as.POSIXlt(start_time)$hour), start_min = unclass(as.POSIXlt(start_time)$min),start_sec = unclass(as.POSIXlt(start_time)$sec)) %>%
     mutate(end_hour = unclass(as.POSIXlt(end_time)$hour), end_min = unclass(as.POSIXlt(end_time)$min),end_sec = unclass(as.POSIXlt(end_time)$sec))
 }
